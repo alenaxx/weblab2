9dead4a1e830efc339797fa759dbea2a5a2382c6
alenaxx <46314154+alenaxx@users.noreply.github.com>
2020-02-07 01:20:22 +0300

    add tests

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css'
+import LargeCityInfo from "./LargeCityInfo";
+import FavoriteCityPack from './FavoriteCityPack'
+import configureStore from '../store/configureStore';
+import {PersistGate} from 'redux-persist/integration/react'
+
+const {store, persistor} = configureStore();
+
+function App() {
+    return (
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-2">Погода здесь</h1>
+            </div>
+            <LargeCityInfo/>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <FavoriteCityPack/>
+                </PersistGate>
+            </Provider>
+        </div>
+    )
+}
+
+
+export default App
